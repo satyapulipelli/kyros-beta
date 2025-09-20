e28d1faa53985558225eee2c2394e61e6e2770fc
Satya Harish Pulipelli <pulipellisatyaharishreddy@gmail.com>
2025-09-20 18:34:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/kyros-beta/',  // Add this line for GitHub Pages
+  plugins: [react()],  // Add this line for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
